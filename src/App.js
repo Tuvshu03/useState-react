9c2fce91c5b8859bec2225596f74d7a960bbea73
Tuvshu03 <159090318+Tuvshu03@users.noreply.github.com>
2024-12-30 12:22:46 +0800

    start react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+// import {state} from react
+function App() {
+//   const [state, statement] = useState(['A', 'B']);
+  return (
+    // <div>this is my state{state}</div>;
+    // <button>{}</button>
+  );
+}
+
+export default App;
